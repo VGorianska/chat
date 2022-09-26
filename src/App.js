df4f4702cfcc131bb99e55a272d3e5afb4a55ee7
Viktoria Gorianska <vika@MacBook-Pro-2.fritz.box>
2022-09-26 10:38:48 +0200

    added chip elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './style.css';
 
 import * as React from 'react';
-import { Card, CardActions, CardContent, IconButton, TextField, Typography } from '@mui/material';
-// import CardMedia from '@mui/material/CardMedia';
+import { Avatar, Card, CardActions, CardContent, CardMedia, IconButton, TextField, Typography } from '@mui/material';
+import { Chip, ChipDelete } from '@mui/joy';
 import { AddReactionOutlined, AttachFileOutlined, Send } from '@mui/icons-material';
 export default function chatCard() {
   return (
@@ -10,16 +10,52 @@ export default function chatCard() {
       width: 415,
       position: "fixed",
       bottom: 20, right: 20,
-      minHeight: "30%",
-      // background: "#4C5270"
+      minHeight: "40%",
     }}>
-      {/* <CardMedia></CardMedia> */}
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Chat
         </Typography>
 
       </CardContent>
+      <CardMedia sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        p: 1,
+        m: 1,
+      }}>
+
+        <Chip
+          variant="outlined"
+          color="neutral"
+          size="lg"
+          startDecorator={<Avatar size="sm" />}
+          endDecorator={<ChipDelete />}
+          sx={{
+            m: 1,
+            p: 1,
+            "--Chip-minHeight": "33px",
+            "--Chip-radius": "25px",
+            "--Chip-gap": "30px"
+          }}
+        >Hallo!</Chip>
+        <Chip
+          variant="outlined"
+          color="neutral"
+          size="lg"
+
+          endDecorator={<Avatar size="sm" />}
+          sx={{
+            m: 1,
+            p: 1,
+            "--Chip-minHeight": "33px",
+            "--Chip-maxWidth": "100px",
+            "--Chip-radius": "25px",
+            "--Chip-gap": "30px"
+          }}
+        >Hey!</Chip>
+
+      </CardMedia>
 
       <CardActions sx={{
         position: "fixed",
